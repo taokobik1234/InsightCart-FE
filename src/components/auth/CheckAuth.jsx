b9087c5263976a53051b3fb885ec51e166a0420b
duckzuybidan <ducduy19012310@gmail.com>
2024-10-25 16:27:15 +0700

    fix navigate

--- src/components/auth/CheckAuth.jsx
+++ src/components/auth/CheckAuth.jsx
@@ -1,8 +1,10 @@
 import { useSelector } from "react-redux"
-import { Navigate } from "react-router-dom"
+import { Navigate, useLocation } from "react-router-dom"
 export default function CheckAuth({children}) {
   const {isAuthenticated} = useSelector(state => state.auth)
-  if(isAuthenticated && window.location.pathname.includes("/auth")) {
+  const location = useLocation()
+  console.log(isAuthenticated, location.pathname.includes("/auth"))
+  if(isAuthenticated && location.pathname.includes("/auth")) {
     return <Navigate to="/" />
   }
   return (
